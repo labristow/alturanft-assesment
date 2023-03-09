8f668d358298a97fc69f25c796743a2bb4da7a99
blessing-quadra <blessing.ladejobi@quadrasystems.org>
2023-03-09 10:34:53 +0100

    deleting context(not used), adding primary(pink&green) colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        "primary-pink": "#F65AE2",
+        "primary-green": "#58D0EA",
+      },
     },
   },
   plugins: [],
